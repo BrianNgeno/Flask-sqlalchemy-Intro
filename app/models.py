3d582d451e9eab3ccea87966518d3f8fe1d0fc85
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2023-10-19 11:38:02 +0300

    updated functions to flask-restful

--- app/models.py
+++ app/models.py
@@ -1,20 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model,SerializerMixin):
     __tablename__='users'
+    serialize_rules = ('-cars.user')
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String,nullable=False, unique=True)
 
     cars = db.relationship('Car',backref='user')
 
     def __repr__(self):
         return f'<The current user id {self.name}>'
 
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+        }
 
-class Car(db.Model):
+
+
+class Car(db.Model,SerializerMixin):
     __tablename__='cars'
+    serialize_rules = ('-user.cars')
+
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String)
 
@@ -22,3 +33,10 @@ class Car(db.Model):
 
     def __repr__(self):
         return f'<The car you are checking is {self.model}>'
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'model':self.model,
+            'user_id': self.user_id,
+        }
